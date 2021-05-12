1ebd4feceb59c8fca1227f2d8ef9c3fb627b5a7c
Vladimir <Luxorchegone@gmail.com>
2021-05-12 11:28:40 +0300

    В компоненте оставлен только меанизм отрисовки, вся логика уехала в компонент hoc-helper

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,50 +1,32 @@
 import React, {Component} from "react";
 import SwapiService from "../../services/swapi-service";
-import Spinner from "../spinner";
-import ErrorIndicator from "../error-indicator";
+import {withData} from "../hoc-helper";
 import "./item-list.css";
 
-export default class ItemList extends Component {
-    state = {
-        itemList: null,
-    }
-
-    onItemsLoaded = (itemList) => { //Пишем в стейт
-        this.setState({
-            itemList: itemList,
-        });
-    }
- 
-    componentDidMount() { //Получаем данные с сервера и пишем в стейт
-        const {getData} = this.props;
-        getData().then(this.onItemsLoaded);
-    }
-
-    renderItems(arr) { //Создаем список для помещения на страницу
+const ItemList = (props) => { //Функция отвечающая за отрисовку компонента
+    const {data, onItemSelected, children: renderLabel} = props;
+   
+    const renderItems = (arr) => { //Создаем список для помещения на страницу
         return arr.map((item) => {
             const {id} = item;
-            const label = this.props.children(item);
+            const label = renderLabel(item);
             return (
                 <li className="list-group-item" 
                 key={id}
-                onClick={() => this.props.onItemSelected(id)}>
+                onClick={() => onItemSelected(id)}>
                     {label}
                 </li>
             );
         });
-    }
+    };
 
-    render() {
-        const {itemList} = this.state;
-        if (!itemList) {
-            return <Spinner />;
-        }
-
-        const items = this.renderItems(itemList);
-        return (
-            <ul className="item-list list-group">
-                {items}
-            </ul>
-        );
-    }
+    const items = renderItems(data);
+    return (
+        <ul className="item-list list-group">
+            {items}
+        </ul>
+    );
 }
+
+const {getAllStarships} = new SwapiService();
+export default withData(ItemList, getAllStarships);
