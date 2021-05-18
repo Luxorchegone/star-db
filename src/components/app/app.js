b95733bd79daa4a8097c8b63e48457a7a7274e33
Vladimir <Luxorchegone@gmail.com>
2021-05-18 14:20:26 +0300

    Фиксируем состояние главного компонента приложения

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,42 @@
 import React, {Component} from "react";
 import Header from "../header";
 import RandomPlanet from "../random-planet";
-import PeoplePage from "../people-page";
+import {PeoplePage, PlanetPage, StarshipPage} from "../pages";
+import {SwapiServiceProvider} from '../swapi-service-context';
+import SwapiService from '../../services/swapi-service';
+import DummySwapiService from '../../services/dummy-swapi-service';
+import ErrorBoundary from '../error-boudary';
 import "./app.css";
 
 export default class App extends Component {
+    state = {
+        swapiService: new SwapiService(),
+    }
+
+    onServiceChange = () => {
+        this.setState(({swapiService}) => {
+        const Service = swapiService instanceof SwapiService ? 
+            DummySwapiService : SwapiService;
+            
+            return {
+                swapiService: new Service()
+            }
+        });
+    }
 
     render() {
         return (
-            <div className="app-wrapper">
-                <Header />
-                <RandomPlanet />
-                <PeoplePage />
-            </div>
+            <ErrorBoundary>
+                <div className="app-wrapper">
+                    <SwapiServiceProvider value={this.state.swapiService}>
+                        <Header onServiceChange={this.onServiceChange}/>
+                        <RandomPlanet />
+                        <PeoplePage />
+                        <PlanetPage />
+                        <StarshipPage />
+                    </SwapiServiceProvider>
+                </div>
+            </ErrorBoundary>
         );
     }                 
 }
