da5089f07ba1cedc15ef2209adeecfdc393be9f7
Vladimir <Luxorchegone@gmail.com>
2021-05-27 23:11:07 +0300

    Добавил коментарии

--- src/components/swapi-service-context/swapi-service-context.js
+++ src/components/swapi-service-context/swapi-service-context.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const { 
+const { //Провайдер и консюмер для пробрасывания сервисов в компоненты приложения
     Provider: SwapiServiceProvider, 
     Consumer: SwapiServiceConsumer
 } = React.createContext();
