d1cf4c127f6b78aed490a93d9f805e1de8a19c07
Vladimir <Luxorchegone@gmail.com>
2021-05-27 23:09:47 +0300

    Небольшие правки форматирования. Добавил коментарии

--- src/components/pages/planet-page.js
+++ src/components/pages/planet-page.js
@@ -1,25 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Row from '../row/';
 import {PlanetDetails, PlanetList} from '../sw-components';
 
-export default class PlanetPage extends Component {
-    state = {
-        selectedItem: null
-    }
+const PlanetPage = ({history, match}) => {
+    const {id} = match.params;
 
-    onItemSelected = (id) => { //Обработчик id выбранного персонажа, который пишем в стейт
-        this.setState({
-        selectedItem: id,
-        });
-    };
-
-    render() {
-        const {selectedItem} = this.state;
-        return (
-            <Row 
-                left={<PlanetList onItemSelected={this.onItemSelected}/>} 
-                right={<PlanetDetails itemId={selectedItem}/>} 
-            /> 
-        );
-    }
+    return (
+        <Row 
+            left={<PlanetList onItemSelected={(itemId)=>{
+                history.push(itemId)
+            }}/>} 
+            right={<PlanetDetails itemId={id}/>} 
+        /> 
+    );
 }
+export default PlanetPage;
