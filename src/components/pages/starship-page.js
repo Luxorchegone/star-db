f397c97a8a75affcbaf52d4adbde01374497157b
Vladimir <Luxorchegone@gmail.com>
2021-05-27 23:10:12 +0300

    Небольшие правки форматирования. Добавил коментарии

--- src/components/pages/starship-page.js
+++ src/components/pages/starship-page.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {StarshipList} from '../sw-components';
 import {withRouter} from 'react-router-dom'
 
-const StarshipPage = ({history}) => {
+const StarshipPage = ({history}) => { //Компонент со списком кораблей и их описанием
     return (
         <StarshipList 
-            onItemSelected={(itemId) => {
-                history.push(itemId);
-            }}/>
+        onItemSelected={(itemId) => {
+            history.push(itemId);
+        }}/>
     );
 }
 export default withRouter(StarshipPage);
