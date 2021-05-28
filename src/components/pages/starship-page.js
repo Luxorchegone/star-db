1fef5aa9f06205fc89c3b0f59d9dd3741d9363c2
Vladimir <Luxorchegone@gmail.com>
2021-05-28 14:48:16 +0300

    Обернул компоненты страницы кораблей в компонент, отлавливающий ошибки

--- src/components/pages/starship-page.js
+++ src/components/pages/starship-page.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {StarshipList} from '../sw-components';
-import {withRouter} from 'react-router-dom'
+import {withRouter} from 'react-router-dom';
+import ErrorBoundary from '../error-boudary';
 
 const StarshipPage = ({history}) => { //Компонент со списком кораблей и их описанием
     return (
-        <StarshipList 
-        onItemSelected={(itemId) => {
-            history.push(itemId);
-        }}/>
+        <ErrorBoundary>
+            <StarshipList onItemSelected={(itemId) => {
+                history.push(itemId);
+            }}/>
+        </ErrorBoundary>
     );
 }
 export default withRouter(StarshipPage);
