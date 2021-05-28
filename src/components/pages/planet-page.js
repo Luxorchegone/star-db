c86195ba22d3b951fd4331c452fa88c043c761ed
Vladimir <Luxorchegone@gmail.com>
2021-05-28 14:48:58 +0300

    Обернул компоненты страницы планет в компонент, отлавливающий ошибки

--- src/components/pages/planet-page.js
+++ src/components/pages/planet-page.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import Row from '../row/';
 import {PlanetDetails, PlanetList} from '../sw-components';
+import ErrorBoundary from '../error-boudary';
 
 const PlanetPage = ({history, match}) => {
     const {id} = match.params;
 
     return (
         <Row 
-            left={<PlanetList onItemSelected={(itemId)=>{
-                history.push(itemId)
-            }}/>} 
-            right={<PlanetDetails itemId={id}/>} 
+            left={
+                <ErrorBoundary>
+                    <PlanetList onItemSelected={(itemId) => {
+                        history.push(itemId);
+                    }}/>
+                </ErrorBoundary>
+            } 
+            right={
+                <ErrorBoundary>
+                    <PlanetDetails itemId={id}/>
+                </ErrorBoundary>
+            } 
         /> 
+        
+        
     );
 }
 export default PlanetPage;
