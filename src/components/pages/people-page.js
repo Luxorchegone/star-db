81db81c05d13cdaaddf645c6de81a9aae0d35cd8
Vladimir <Luxorchegone@gmail.com>
2021-05-28 14:47:25 +0300

    Обернул компоненты страницы персонажей в компонент, отлавливающий ошибки

--- src/components/pages/people-page.js
+++ src/components/pages/people-page.js
@@ -2,15 +2,24 @@ import React from 'react';
 import Row from '../row/';
 import {PersonDetails, PersonList} from '../sw-components';
 import {withRouter} from 'react-router-dom';
+import ErrorBoundary from '../error-boudary';
 
 const PeoplePage = ({history, match}) => { //Компонент со списком персонажей и их описанием
     const {id} = match.params;    
     return (
         <Row 
-            left={<PersonList onItemSelected={(itemId) => {
-                history.push(itemId);
-            }}/>} 
-            right={<PersonDetails itemId={id}/>} 
+            left={
+                <ErrorBoundary>
+                    <PersonList onItemSelected={(itemId) => {
+                        history.push(itemId);
+                    }}/>
+                </ErrorBoundary>
+            } 
+            right={
+                <ErrorBoundary>
+                    <PersonDetails itemId={id}/>
+                </ErrorBoundary>
+            } 
         /> 
     );
 }
